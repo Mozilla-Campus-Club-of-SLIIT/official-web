a1203fd3f43a36a4646a3305bf3ee7f750d20bce
MrVirul <virul.mmeemana@gmail.com>
2025-12-22 04:12:40 +0530

    feat: add new webinar

--- data/webinar.ts
+++ data/webinar.ts
@@ -40,6 +40,16 @@ export const webinars: Webinar[] = [
     type: "playlist",
   },
   {
+    title: "Getting started with bootstrap",
+    Thumbnail: "https://img.youtube.com/vi/DF2AVprXvkg/maxresdefault.jpg",
+    description:
+      "Learn the basics of Bootstrap and how to build simple, user-friendly web pages with great UX.",
+    speaker: "Shehan Silva",
+    videoCount: 3,
+    link: "https://www.youtube.com/playlist?list=PLkWgPcG-GFhBfp15nmhJRJYaPxuxg12Xz",
+    type: "playlist",
+  },
+  {
     title: "Introduction to Google Crowdsource",
     Thumbnail: "https://img.youtube.com/vi/bDyJM38wVb0/maxresdefault.jpg",
     description:
@@ -62,10 +72,11 @@ export const webinars: Webinar[] = [
   {
     title: "Building and Deploying RESTful API with Go",
     Thumbnail: "https://img.youtube.com/vi/Gz4b2f7qwyg/maxresdefault.jpg",
-    description: "",
-    speaker: "",
+    description:
+      "This video introduces Go, a statically typed programming language from Google, highlighting its syntax, concurrency features, and memory safety.",
+    speaker: "Tharindu Balasooriya",
     videoCount: 0,
-    link: "",
+    link: "https://www.youtube.com/watch?v=Gz4b2f7qwyg&t=505s",
     type: "video",
   },
 
@@ -77,6 +88,66 @@ export const webinars: Webinar[] = [
     speaker: "Sanuja Methmal",
     videoCount: 0,
     link: "https://www.youtube.com/watch?v=FUczB6yMeP0&list=PLkWgPcG-GFhC_3CLtupI4O4bvpNFWdBU2&index=2",
+    type: "video",
+  },
+  {
+    title: "AI Bot Technology",
+    Thumbnail: "https://img.youtube.com/vi/4MVQUVSGRRY/maxresdefault.jpg",
+    description:
+      "An AI chatbot (Artificial Intelligence chatbot) is a chatbot that's powered by artificial intelligence (AI).",
+    speaker: "Shashika Kahatapitiya",
+    videoCount: 0,
+    link: "https://www.youtube.com/watch?v=4MVQUVSGRRY&list=PLkWgPcG-GFhBHAPwY9KLxo-6gUYRR2ItX&index=4",
+    type: "video",
+  },
+  {
+    title: "Emergence of Deepfake Technology",
+    Thumbnail: "https://img.youtube.com/vi/pJ9XbMcHiuA/maxresdefault.jpg",
+    description:
+      "This video introduces Bootstrap and shows how to create simple, user-friendly web pages using its components and layout system.",
+    speaker: "Pasindu Bandara",
+    videoCount: 0,
+    link: "https://www.youtube.com/watch?v=pJ9XbMcHiuA&list=PLkWgPcG-GFhBHAPwY9KLxo-6gUYRR2ItX&index=5",
+    type: "video",
+  },
+  {
+    title: "Java Scripts",
+    Thumbnail: "https://img.youtube.com/vi/MbXFm2wfjLY/maxresdefault.jpg",
+    description:
+      "This video introduces JavaScript, covering its basics, syntax, and key features for building dynamic web applications.",
+    speaker: "Chamathka Ariyarathna",
+    videoCount: 0,
+    link: "https://www.youtube.com/watch?v=MbXFm2wfjLY&list=PLkWgPcG-GFhBHAPwY9KLxo-6gUYRR2ItX&index=6",
+    type: "video",
+  },
+  {
+    title: "New Technological Trends",
+    Thumbnail: "https://img.youtube.com/vi/LmdpDTDJbec/maxresdefault.jpg",
+    description:
+      "This video explores how rapidly evolving technology and COVID-19 have changed the role of IT professionals in today’s contactless world.",
+    speaker: "K.D. Divya",
+    videoCount: 0,
+    link: "https://www.youtube.com/watch?v=LmdpDTDJbec&list=PLkWgPcG-GFhBHAPwY9KLxo-6gUYRR2ItX&index=7",
+    type: "video",
+  },
+  {
+    title: "Raspberry Pi for Computer Vision",
+    Thumbnail: "https://img.youtube.com/vi/tb4znRmig0U/maxresdefault.jpg",
+    description:
+      "This video introduces Raspberry Pi, a small single-board computer designed to teach computer science and enable practical projects.",
+    speaker: "Saluka Udbhasa",
+    videoCount: 0,
+    link: "https://www.youtube.com/watch?v=tb4znRmig0U",
+    type: "video",
+  },
+  {
+    title: "Quick Guide to Quine",
+    Thumbnail: "https://img.youtube.com/vi/mNP35Ey35ZQ/maxresdefault.jpg",
+    description:
+      "This video shows how to set up a Quine account and explore repositories quickly on this user-friendly platform.",
+    speaker: "David Butler ",
+    videoCount: 0,
+    link: "https://www.youtube.com/watch?v=mNP35Ey35ZQ",
     type: "video",
   },
 ]
